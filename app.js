743f0496bc85283da0ba489c79adf9c643141c11
Mitch Ball <mitchb145@gmail.com>
2017-07-29 14:16:17 +1000

    Add dotenv

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const apiai = require('apiai');
+require('dotenv').config()
 
 
 // Custom Dependencies
