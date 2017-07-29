871f7db43248b34230ebc9a152be72b3bd4a2d4b
Mitch Ball <mitchb145@gmail.com>
2017-07-29 17:29:59 +1000

    Remove babel

--- translate.js
+++ translate.js
@@ -1,7 +1,7 @@
 // Vendor Dependencies
-import fetch from "isomorphic-fetch"
+const fetch = require('isomorphic-fetch')
 
-export const translate = async (req, res, message, target="en") => {
+const translate = async (req, res, message, target="en") => {
   const response = await fetch("https://translation.googleapis.com/language/translate/v2?key=AIzaSyBKO65TgERUuR8Njhp1JHckjaHOWKFVz78&q="+message+"&target="+target);
   const resData = await response.json();
   const data = resData.data.translations[0]
@@ -9,4 +9,8 @@ export const translate = async (req, res, message, target="en") => {
   const text = data.translatedText
   const lang = data.detectedSourceLanguage
   return { text, lang }
+}
+
+module.exports = {
+  translate
 }
