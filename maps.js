a9aab197afe4b38ab2d186faad4b499a49b40e96
Linton Ball <me@lintonball.com>
2017-07-30 10:59:08 +1000

    maps integration

---
+++ maps.js
@@ -0,0 +1,25 @@
+// Vendor Dependencies
+const fetch = require('isomorphic-fetch')
+
+
+
+const findRoute = (origin, destination) => {
+  const url = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key=AIzaSyCqtw00-eflkY-AKPzmyl281c2x4K56pUQ"
+  return fetch(url).then((response) => {
+    return response.json()
+  }).then((data) => {
+    const routes = []
+    data.routes[0].legs[0].steps.forEach((val) => {
+      routes.push(val.html_instructions.replace(/(<([^>]+)>)/ig, ""))
+    })
+    return(routes)
+  })
+}
+
+const getCoords = () => {
+
+}
+
+module.exports = {
+  findRoute
+}
